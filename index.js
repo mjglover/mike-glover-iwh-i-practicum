55e9ee08cf624a42a0f64be1fb8676bfdeb04dc1
Mike Glover <mjglover@gmail.com>
2026-09-13 10:33:22 -0400

    Add POST route to create a pet in HubSpot and redirect home

--- index.js
+++ index.js
@@ -43,6 +43,29 @@ app.get('/update-cobj', (req, res) => {
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newPet = {
+        properties: {
+            name: req.body.name,
+            type: req.body.type,
+            color: req.body.color
+        }
+    };
+
+    const createPet = `https://api.hubapi.com/crm/v3/objects/${PETS_OBJECT_TYPE}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(createPet, newPet, { headers });
+        res.redirect('/');
+    } catch (err) {
+        console.error(err.response ? err.response.data : err);
+        res.status(500).send('Could not create the pet in HubSpot.');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
