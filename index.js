a1ceb0337d8703f1ee30eff0f441d1bf0394c229
Mike Glover <mjglover@gmail.com>
2026-09-13 10:31:58 -0400

    Load private app token from .env and add npm start script

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// The token is read from a git-ignored .env file (see the start script in package.json).
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
