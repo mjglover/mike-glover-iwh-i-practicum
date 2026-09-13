89d9243e4cd5e366e31d3d617a2d7692c2d73ee5
Mike Glover <mjglover@gmail.com>
2026-09-13 10:32:33 -0400

    Add homepage route and template listing pets from HubSpot

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+const PETS_OBJECT_TYPE = '2-250700368';
+const PET_PROPERTIES = ['name', 'type', 'color'];
+
+app.get('/', async (req, res) => {
+    const pets = `https://api.hubapi.com/crm/v3/objects/${PETS_OBJECT_TYPE}?properties=${PET_PROPERTIES.join(',')}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        const resp = await axios.get(pets, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Pets | Integrating With HubSpot I Practicum', data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Could not load pets from HubSpot.');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
